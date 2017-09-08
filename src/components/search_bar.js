046344838bb60491979ec2e5741b9876070a5ea5
Kevin <cho.kevin@hotmail.com>
2017-09-08 12:51:30 +0800

    youtube api search response works

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+
+  constructor(props){
+    // calling parent component class methods
+    super(props);
+
+    // this.state only exists in constructor
+    this.state = {term: 'Starting Value'}
+  }
+
   render() {
-    return <input />
+    // BAD == this.state.term = event.target.value == BAD
+    return (
+      <div>
+        <input
+          value = {this.state.term}
+          onChange={ event => this.setState({term:event.target.value})}
+        />
+      </div>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 // React has diverged into two seperate libraries
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 
-const API_KEY = 'AIzaSyCSn2OhAV4kiz6-HaygJRoksRTaymSSMKsx'
+const API_KEY = 'AIzaSyCSn2OhAV4kiz6-HaygJRoksRTaymSSMKs'
+
+YTSearch({key:API_KEY,term:'surfboards'}, function(data){
+  console.log(data);
+})
 
 // for multiline JS6 must use ()
+// if Const App is a functional Component because it does not have any concept of state and is not a class
 const App = () => {
   return (
     <div>
